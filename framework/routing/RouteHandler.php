784c45fb111b55093bff1e51e7c766438181b2aa
Netlic <netlic@centrum.sk>
2016-06-12 22:55:39 +0200

    removed tons of comments

--- framework/routing/RouteHandler.php
+++ framework/routing/RouteHandler.php
@@ -29,7 +29,6 @@ class RouteHandler {
 	
 	public static function findRoute(){
 		$route = Core::engine()->request->get();
-		//$routes = [];
 		if(!empty($route)){
 			$rdirs = [];
 			if(static::isRouteDirective(array_keys($route), $rdirs)){
@@ -45,7 +44,7 @@ class RouteHandler {
 		}else{
 			static::$routes[] = static::defaultRoute();
 		}
-		$controllerClass = static::findController(/*$routes*/);
+		$controllerClass = static::findController();
 		//tu bude v buducnosti hodeny error, ak nenajde controller
 	}
 	
@@ -61,57 +60,12 @@ class RouteHandler {
 		return !empty($rdirs);
 	}
 	
-    public function redirect(/*$route = null, $defaultRouting = true*/){
+    public function redirect(){
+		//error, ak neexistuje static::routes
 		$routes = static::$routes;
 		$firstRoute = strlen(array_keys(reset($routes))) > 0 ? array_keys(reset($routes)) : "none";
+		//error, ak nie je controller
 		Core::engine()->addContent(static::$controller->$firstRoute());
 		Core::$document->dom->appendHtml(static::$controller->LoadLayout());
-		
-		/*$requestSchema = $this->findRoute($route);
-		$controllerRoute = $this->findControllerRoute($requestSchema ?? []);
-		$this->controller = new Controller();
-		if($defaultRouting){
-			ChladnickaSettings::engine()->addContent($this->readContent($controllerRoute, $route, $requestSchema));
-			ChladnickaSettings::engine()->addBodyContent($this->controller->loadLayout());
-		}else{
-			$this->readContent($controllerRoute, $route, $requestSchema);
-		}*/
-    }
-    
-    private function readContent(/*$cRoute = null, $route = null, $requestSchema = null*/){
-		/*$publicViews = RouteSchema::publicViews();
-		if(!$route){
-			$defView = RouteSchema::returnDefaultView();
-			$defKeys = array_keys($defView);
-			if(!empty($defKeys)){
-				$method = reset($defKeys);
-				$instantiate = str_replace("Controller",reset($defView),get_class($this->controller));
-				$controllerInstance = new $instantiate();
-				return call_user_func_array([$controllerInstance, $method],[]);
-			}
-			return null;
-		}
-		$view = $requestSchema["humanName"] ?? $requestSchema["method"];
-		if(in_array($route, $publicViews) || ChladnickaSettings::user()->isAuthorised($view)){
-			$instantiate = str_replace("Controller", $cRoute, get_class($this->controller));
-			return call_user_func_array([(new $instantiate()),$requestSchema["method"]], $requestSchema["params"]);
-		}
-		$unauthorised = RouteSchema::returnUnathorised();
-		return call_user_func_array([reset(array_keys($unauthorised)), reset($unauthorised)]);
-		*/
-    }
-    
-    //private function findRoute(/*$route = null*/){
-		/*if(!$route){
-			return $route;
-		}
-		return RouteSchema::requestSchema()[$route] ?? ["method" => $route, "params" => []];*/
-    //}
-    
-    private function findControllerRoute(/*array $requestSchema = []*/){
-		/*if(empty($requestSchema)){
-			return null;
-		}
-		return RouteSchema::returnRoute()[$requestSchema["method"]] ?? null;*/
     }
 }
